9d983db5d25f4aac7d9ea6674213737c6e79509a
onSTRANN1Y <splashilia3@gmail.com>
2024-11-13 22:24:07 +0300

    add flake8 in group dev, change say.hello.py

--- testgit/say_hello.py
+++ testgit/say_hello.py
@@ -1,9 +1,12 @@
 from colorama import init, Fore, Back, Style
 
+
 init()
+
 
 def main():
     print(Fore.RED + Back.GREEN + Style.BRIGHT + 'Hello world!')
+
 
 if __name__ == '__main__':
     main()
